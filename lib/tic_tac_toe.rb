ea1eacfbbbc2e9df37b5a7ed2b24426e89cfd652
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-08 12:11:32 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -128,7 +128,14 @@ class TicTacToe
   end
 
   def winner?
-
+    if over?
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return "X"
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return "O"
+      end
+    end
   end
 
 
